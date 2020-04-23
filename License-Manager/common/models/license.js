2499701b74aa3a5ce544cc48f261fdbb597b3e54
dthompson12315 <dthompson2016@my.fit.edu>
2020-04-23 13:37:19 -0400

    formatting

--- License-Manager/common/models/license.js
+++ License-Manager/common/models/license.js
@@ -17,39 +17,13 @@ module.exports = function(License) {
         next();
     });
 
-    License.status = function(license_n, cb) {
-        var RandomOrg = require('random-org');
-        var random = new RandomOrg({ apiKey: "64df9e64-d7ff-462b-9813-3e6608744d3e" });
-        random.generateStrings({ n: license_n, length: 25, characters: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" })
-        .then(function(result) {
-            cb(null, result.random.data);
-        });
-        
-    };
 
-    License.remoteMethod(
-        'status', {
-          http: {
-            path: '/status',
-            verb: 'post'
-          },
-          accepts: {
-              arg: 'license_n',
-              type: 'number'
-          },
-          returns: {
-            arg: 'licenses',
-            type: [ 'string' ]
-          }
-        }
-      );
-
-      License.verify = function(license_key, product_s, cb) {
+    License.verify = function(license_key, product_s, cb) {
         let filter={
             where: {
                     licenses: license_key,
                     product: product_s
-                   },
+                    },
         }
         License.find(filter, function(license_model) {
             if (license_model) {
@@ -64,28 +38,27 @@ module.exports = function(License) {
             }
             cb(null, false);
         });
-        
-      }
+    
+    }
 
-      License.remoteMethod(
-        'verify', {
-          http: {
+    License.remoteMethod('verify', {
+        http: {
             path: '/verify',
             verb: 'get'
-          },
-          accepts: [ {
-              arg: 'license_key',
-              type: 'string'
-             },
-             {
-                 arg:'product',
-                 type: 'string'
-             }
-            ],
-          returns: {
+        },
+        accepts: [ 
+            {
+                arg: 'license_key',
+                type: 'string'
+            },
+            {
+                arg:'product',
+                type: 'string'
+            }
+        ],
+        returns: {
             arg: 'verified',
             type: 'Boolean'
-          }
         }
-      );
+    });
 };
